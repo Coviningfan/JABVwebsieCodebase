f3900b69ffecec6f9e5fd141a1d30da3673d2205
Joseph Berardi <81837387+Coviningfan@users.noreply.github.com>
2025-06-26 17:40:08 +0000

    made it work

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://qzfcefvusjzdzseokdla.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6ZmNlZnZ1c2p6ZHpzZW9rZGxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ0NTcyMzMsImV4cCI6MjA1MDAzMzIzM30.BQ1v9SqJQUXjcjmAJbBmGOPPwPKqn1EWKuR7r-Q9CX4';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
