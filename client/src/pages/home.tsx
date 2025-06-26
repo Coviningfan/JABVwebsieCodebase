49c3ebd53122ea8adc3802b9a5ccf5cac0bba952
Joseph Berardi <81837387+Coviningfan@users.noreply.github.com>
2025-06-26 01:11:54 -0700

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,5 @@
 import { Navigation } from '@/components/navigation';
-import { HeroSection } from '@/components/hero-section';
+import HeroSection from '@/components/hero-section';
 import { PortfolioCarousel } from '@/components/portfolio-carousel';
 import { ServicesSection } from '@/components/services-section';
 import { AboutSection } from '@/components/about-section';
