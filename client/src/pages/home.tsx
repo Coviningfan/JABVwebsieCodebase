38c0c300135af5bb966f952d5cbe6f45ddd77842
Joseph Berardi <81837387+Coviningfan@users.noreply.github.com>
2025-06-26 04:27:52 -0700

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,5 @@
 import { Navigation } from '@/components/navigation';
-import { HeroSection } from '@/components/hero-section';
+import { HeroWithBanner } from '@/components/hero-section'; // Changed from HeroSection to HeroWithBanner
 import { PortfolioCarousel } from '@/components/portfolio-carousel';
 import { ServicesSection } from '@/components/services-section';
 import { AboutSection } from '@/components/about-section';
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white">
       <Navigation />
-      <HeroSection />
+      <HeroWithBanner /> {/* Changed from HeroSection to HeroWithBanner */}
       <PortfolioCarousel />
       <ServicesSection />
       <AboutSection />
