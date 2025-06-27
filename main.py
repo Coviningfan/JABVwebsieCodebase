a8869476e36047cb1f666b0d060b3fc1b21aff3c
Joseph Berardi <81837387+Coviningfan@users.noreply.github.com>
2025-06-27 15:33:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import psycopg2
 
 app = FastAPI()
 
-DATABASE_URL = os.environ["https://qzfcefvusjzdzseokdla.supabase.co"]
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 @app.get("/api/cost-rates")
 def get_cost_rates():
