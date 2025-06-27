2a3a8457a690091274a41f4a49b50f46dd3e1cf9
Joseph Berardi <81837387+Coviningfan@users.noreply.github.com>
2025-06-26 19:34:48 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from fastapi import FastAPI
+import psycopg2
+
+app = FastAPI()
+
+DATABASE_URL = os.environ["https://qzfcefvusjzdzseokdla.supabase.co"]
+
+@app.get("/api/cost-rates")
+def get_cost_rates():
+    conn = psycopg2.connect(DATABASE_URL)
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM cost_rates;")
+        rows = cur.fetchall()
+    conn.close()
+    return rows
